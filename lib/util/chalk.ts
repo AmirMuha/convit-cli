6ee017f44c6b9a8e26b485cf0f85fce8a3550447
amirmuha <mirzaii4046@gmail.com>
2021-12-31 16:49:51 +0330

    feat: added chalk colors for logging

---
+++ lib/util/chalk.ts
@@ -0,0 +1,15 @@
+import chalk from "chalk"
+export const yellow = chalk.hex("#ffea00");
+export const red = chalk.red;
+export const blue = chalk.hex("#0288d1");
+export const black = chalk.hex("#000");
+export const white = chalk.hex("#fff");
+export const green = chalk.hex("#00c853");
+export const gray = chalk.hex("#546e7a");
+export const bgYellow = chalk.hex("#ffea00");
+export const bgRed = chalk.red;
+export const bgBlue = chalk.hex("#0288d1");
+export const bgBlack = chalk.hex("#000");
+export const bgWhite = chalk.hex("#fff");
+export const bgGreen = chalk.hex("#00c853");
+export const bgGray = chalk.hex("#546e7a");
