aff9427698418094b5c8329df02ad72af2b3289d
amirmuha <mirzaii4046@gmail.com>
2022-01-02 02:02:24 +0330

    feat: added types

---
+++ lib/types.ts
@@ -0,0 +1,18 @@
+export type SupportedImageOutput =
+  | "png"
+  | "jpeg"
+  | "webp"
+  | "tiff"
+  | "avif";
+export type AllFileTypes =
+  | "png"
+  | "jpeg"
+  | "webp"
+  | "tiff"
+  | "avif"
+  | "svg"
+  | "jpg"
+  | "gif"
+  | "mp4"
+  | "mkv";
+
