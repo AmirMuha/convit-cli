4a903721eae8f705fe70ac827c3ffe3799c550eb
amirmuha <mirzaii4046@gmail.com>
2022-01-01 21:30:31 +0330

    fix: fixed getExtension format prop type

--- lib/util/regexp.ts
+++ lib/util/regexp.ts
@@ -1,6 +1,8 @@
+import {AllFileTypes} from "../types";
+
 export const getFilename = /(([^\/])+\.(\w+))/g;
 
-export const getExtension = (format: string) => {
+export const getExtension = (format: AllFileTypes) => {
   return new RegExp(`\.(${format})$`,"ig")
 }
 
