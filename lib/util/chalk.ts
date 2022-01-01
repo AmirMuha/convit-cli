e0b2d022901646860f9ed3d57946341ca85fd71d
amirmuha <mirzaii4046@gmail.com>
2022-01-01 20:50:54 +0330

    feat: added colors for debuging

--- lib/util/chalk.ts
+++ lib/util/chalk.ts
@@ -1,6 +1,7 @@
 import chalk from "chalk"
 export const yellow = chalk.hex("#ffea00");
 export const red = chalk.red;
+export const orange = chalk.hex("#F38330")
 export const blue = chalk.hex("#0288d1");
 export const black = chalk.hex("#000");
 export const white = chalk.hex("#fff");
@@ -8,6 +9,7 @@ export const green = chalk.hex("#00c853");
 export const gray = chalk.hex("#546e7a");
 export const bgYellow = chalk.hex("#ffea00");
 export const bgRed = chalk.red;
+export const bgOrange = chalk.hex("$F38330")
 export const bgBlue = chalk.hex("#0288d1");
 export const bgBlack = chalk.hex("#000");
 export const bgWhite = chalk.hex("#fff");
