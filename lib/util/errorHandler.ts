27a49410ea196a0892507d27307de39472a888a2
amirmuha <mirzaii4046@gmail.com>
2022-01-01 21:29:36 +0330

    fix: updated ERROR to Error

--- lib/util/errorHandler.ts
+++ lib/util/errorHandler.ts
@@ -3,6 +3,6 @@ export default (fn: (...args: any[]) => void):any => {
   try {
     return fn()
   } catch(e) {
-    console.log(red("ERROR: ",e.message))
+    console.log(red("Error: ",e.message))
   }
 }
