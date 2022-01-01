5e80aaeb65e4143740c66e865a989ab359a522b9
amirmuha <mirzaii4046@gmail.com>
2022-01-02 02:13:48 +0330

    update: seperated concerns -> moved gathering files to this file

--- lib/util/getFilesByFormat.ts
+++ lib/util/getFilesByFormat.ts
@@ -5,6 +5,9 @@ import {
 } from "./debug"
 import isDir from "../check/isDir";
 import {AllFileTypes} from "../types";
+import isImage from "../check/isImage";
+import isFileExist from "../check/isFileExist";
+
 
 /**
  * @description use this function to find all files corresponding to a specific format.
@@ -29,7 +32,7 @@ import {AllFileTypes} from "../types";
  *
  *
 */
-const getFilesByFormat =(options: {
+export const getFilesByFormatInOneDir = (options: {
   directory: string;
   format: AllFileTypes;
 }): string[]=> {
@@ -60,4 +63,58 @@ const getFilesByFormat =(options: {
   return matchedFiles;
 }
 
-export default getFilesByFormat
+/**
+ * @description gather all files with simillar format in several directories.
+ *
+ * @param options 
+ * ```js
+ * options {
+ *    inputs: string[]
+ *    format: AllFileTypes
+ * }
+ * ```
+ *
+ * @example 
+ * ```js
+ * getFilesByFormatInSeveralDirs({
+ *    inputs: [
+ *      "/files/image-1.jpg",
+ *      "/files/image-2.jpg",
+ *      "/files/image-3.jpg",
+ *      "/files/icons", // directory (or /files/icons/)
+ *    ],
+ *    format: "png"
+ * })
+ * ```
+*/
+export const getFilesByFormatInSeveralDirs = (options:{
+  inputs:string[]
+  format: AllFileTypes
+}):string[] => {
+  const {
+    inputs,
+    format
+  } = options;
+  const files:string[] = [];
+  inputs.forEach((i) => {
+    if(isImage(i)) {
+      if(isFileExist(i)) {
+        files.push(i)
+      } else {
+        console.error(red(`${i} file doesn't exist. Perhaps deleted before being converted.`))
+      }
+    } else if(isDir(i)) {
+      files.concat(
+        getFilesByFormatInOneDir({
+          directory: i,
+          format,
+        })
+      );
+    } else {
+      console.error(red(`Input must be either a file or directory.`))
+      return
+    }
+  });
+  return files
+}
+
