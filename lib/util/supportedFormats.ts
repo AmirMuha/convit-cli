680a86aa0663eb49f9c3d9262d45cf0a1077827e
amirmuha <mirzaii4046@gmail.com>
2022-01-02 23:19:28 +0330

    feat: seperated supported formats from conver/image.ts file

---
+++ lib/util/supportedFormats.ts
@@ -0,0 +1,3 @@
+// ------------------ Supported Image Types
+export const supportedInputImages = ["svg","png","jpeg","webp","tiff","jpg","avif","gif"]
+export const supportedOutputImages = ["png","jpeg","webp","tiff","jpg","avif"]
