be2f7a91b14850be03b6350454daff9f7d04c250
amirmuha <mirzaii4046@gmail.com>
2022-01-04 01:32:05 +0330

    fix: fixed lastDir regexp

--- lib/util/regexp.ts
+++ lib/util/regexp.ts
@@ -2,7 +2,7 @@ import {AllFileTypes} from "../types";
 
 export const allFilenamesRegExp = /(([^\/])+\.(\w+))/gi;
 export const lastFilenameRegExp = /(([^\/])+\.(\w+))$/gi;
-export const lastDirRegExp = /([^\/]+[\/]?)$/gi;
+export const lastDirRegExp = /[^\/][a-zA-Z0-9_-]+[\/]?$/gi;
 export const extensionRegExp = (format: AllFileTypes) => {
   return new RegExp(`\.(${format})$`,"ig")
 }
