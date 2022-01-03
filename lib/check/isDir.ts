0b6f2f5f4ed278c2192d90ffe4b181ada0792931
amirmuha <mirzaii4046@gmail.com>
2022-01-03 18:04:10 +0330

    fix: added checking if exist before checking if it is a directory

--- lib/check/isDir.ts
+++ lib/check/isDir.ts
@@ -1,9 +1,12 @@
 import fs from "fs"
 export default (inputPath: string):boolean => {
   let isDir = false;
-  const stats = fs.statSync(inputPath);
-  if (stats.isDirectory()) {
-    isDir = true;
+  const isExist = fs.existsSync(inputPath);
+  if (isExist) {
+    const stats = fs.statSync(inputPath);
+    if (stats.isDirectory()) {
+      isDir = true;
+    }
   }
   return isDir;
 }
