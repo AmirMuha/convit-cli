bf3888f1283d3937d646735c6dd5841a7f1199d2
amirmuha <mirzaii4046@gmail.com>
2022-01-03 14:52:29 +0330

    update: changed format question name into convertTo, refactor

--- lib/util/interactiveHandler.ts
+++ lib/util/interactiveHandler.ts
@@ -9,90 +9,92 @@ import inquirerFuzzyPath from "inquirer-fuzzy-path"
 //------------REGISTERING INQUIRER PLUGIN
 inquirer.registerPrompt('fuzzypath', inquirerFuzzyPath);
 //------------QUESTIONS
-export const questions:QuestionCollection = [
-    {
-      name: "files",
-      type: "input",
-      message: `${red(
-        `Enter the files you want to manipulate ${orange(
-          "(required, seperate files with comma)"
-        )} \n example -> ./file.ext, ./dir: `
-      )}`,
-      filter(input: string) {
-        const files = input.split(",");
-        const resolvedFiles: string[] = [];
-        if(!files[0]) {
-          throw new Error("No files or directories specified.")
+export const questions: QuestionCollection = [
+  {
+    name: "files",
+    type: "input",
+    message: `${red(
+      `Enter the files you want to manipulate ${orange(
+        "(required, seperate files with comma)"
+      )} \n example -> ./file.ext, ./dir: `
+    )}`,
+    filter(input: string) {
+      const files = input.split(",");
+      const resolvedFiles: string[] = [];
+      if (!files[0]) {
+        throw new Error("No files or directories specified.");
+      }
+      files.forEach((f) => {
+        if (fs.existsSync(getResolvedPath(f))) {
+          resolvedFiles.push(getResolvedPath(f.trim()));
+        } else {
+          throw new Error(`file "${f}" doesn't exist.`);
         }
-        files.forEach((f) => {
-          if(fs.existsSync(getResolvedPath(f))) {
-            resolvedFiles.push(getResolvedPath(f.trim()));
-          } else {
-            throw new Error(`file "${f}" doesn't exist.`)
-          }
-        });
-        return resolvedFiles;
-      },
+      });
+      return resolvedFiles;
     },
-    {
-      name: "format",
-      type: "list",
-      message: `${red(
-        `Select the format you want to convert your files into ? ${orange(
-          "(required)"
-        )} `
-      )}`,
-      choices: supportedOutputImages,
-    },
-    {
-      name: "output",
-      type: "string",
-      message: `${red(
-        `Enter the output path where you want to save converted file there ${orange(
-          "(optional, default -> current directory with the input filename)"
-        )}\n example -> ./output.ext or ./outputDir:`
-      )} `,
-      filter(input: string) {
-        input = getResolvedPath(input.trim())
-        return input
-      },
-    },
-    {
-      name: "width",
-      type: "number",
-      message: `${red(
-        `Enter the width of the output file in pixel ${orange(
-          "(optional)"
-        )}\nexample -> 1200:`
-      )} `,
-      filter(input: number) {
+  },
+  {
+    name: "convertTo",
+    type: "list",
+    message: `${red(
+      `Select the format you want to convert your files into ? ${orange(
+        "(required)"
+      )} `
+    )}`,
+    choices: supportedOutputImages,
+  },
+  {
+    name: "output",
+    type: "string",
+    message: `${red(
+      `Enter the output path where you want to save converted file there ${orange(
+        "(optional, default -> current directory with the input filename)"
+      )}\n example -> ./output.ext or ./outputDir:`
+    )} `,
+    filter(input: string) {
+      if (input) {
+        input = getResolvedPath(input.trim());
         return input;
-      },
+      }
     },
-    {
-      name: "height",
-      type: "number",
-      message: `${red(
-        `Enter the height of the output file in pixel ${orange(
-          "(optional)"
-        )}\nexample -> 1200: `
-      )}`,
-      filter(input: number) {
-        return input;
-      },
+  },
+  {
+    name: "width",
+    type: "number",
+    message: `${red(
+      `Enter the width of the output file in pixel ${orange(
+        "(optional)"
+      )}\nexample -> 1200:`
+    )} `,
+    filter(input: number) {
+      return input;
     },
-    {
-      name: "quality",
-      type: "number",
-      message: `${red(
-        `Enter the quality of the output file ${orange(
-          "(optional, valid range -> 0 to 100)"
-        )}\nexample -> 60: `
-      )}`,
-      filter(input: number) {
-        return input;
-      },
+  },
+  {
+    name: "height",
+    type: "number",
+    message: `${red(
+      `Enter the height of the output file in pixel ${orange(
+        "(optional)"
+      )}\nexample -> 1200: `
+    )}`,
+    filter(input: number) {
+      return input;
     },
+  },
+  {
+    name: "quality",
+    type: "number",
+    message: `${red(
+      `Enter the quality of the output file ${orange(
+        "(optional, valid range -> 0 to 100)"
+      )}\nexample -> 60: `
+    )}`,
+    filter(input: number) {
+      return input;
+    },
+  },
 ];
 // -------------SETTING UP INQUIRER
 export default async () => {
