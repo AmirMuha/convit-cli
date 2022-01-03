d6d5e330232271b7f5d60c4469db130f4eeebd1e
amirmuha <mirzaii4046@gmail.com>
2022-01-04 01:27:29 +0330

    fix: fixed changing output path in different situations

--- lib/convert/image.ts
+++ lib/convert/image.ts
@@ -6,11 +6,15 @@ import isOutputValid from "../check/isOutputValid";
 import { getFilesByFormatInSeveralDirs } from "../util/getFilesByFormat";
 import { debugConvertImage as debug } from "../util/debug";
 import { supportedInputImages } from "../util/supportedFormats";
+import { lastFilenameRegExp } from "../util/regexp"
+import fs from "fs"
+import getRandomName from "../util/getRandomName";
+import getResolvedPath from "../util/getResolvedPath";
 
 // ------------------ CONVERT IMAGES WITH SHARP
 const convert = (options: {
   input: string;
-  output: string;
+  output?: string;
   format: SupportedImageOutput;
   width?: number;
   height?: number;
@@ -30,16 +34,41 @@ const convert = (options: {
     progressive,
     filename,
   } = options;
+    const isFilenameExist = fs.existsSync(input.replace(/(\.[a-zA-Z0-9-_]+)$/i, "." + format));
+  if (!!output) {
+    if (isFilenameExist) {
+      output =
+        output +
+        "/" +
+        getRandomName({
+          format,
+          prefix: "image",
+        });
+    } else {
+      output =
+        output +
+        "/" +
+        input.match(lastFilenameRegExp)![0].replace(/(\.\w+)$/i, "." + format);
+    }
+  } else {
+    if (isFilenameExist) {
+      output = getResolvedPath(getRandomName({ format, prefix: "image" }));
+    } else {
+      output = getResolvedPath(
+        input.match(lastFilenameRegExp)![0].split(".")[0] + "." + format
+      );
+    }
+  }
+  debug(red("Final output: ")+ gray(output))
   sharp(input)
     .toFormat(format, { quality, progressive, compressionLevel })
     .resize(width, height)
-    .toFile(output)
+    .toFile(output!)
     .then((info) => {
-      console.log(info);
-      debug(green("Successful! "), gray(`Output: ${output}\n`));
+      console.log(green("Successful! "), gray(`Output: ${output}\n`));
     })
     .catch((e) => {
-      console.error(red("Error: "), gray(e.message));
+      console.log(red("Error: "), gray(e.message));
     });
 };
 
